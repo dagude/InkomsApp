8efa405887210e004792d1675418b043e839421b
David Agudelo <dagude14@hotmail.com>
2022-06-23 13:53:24 -0400

    try deploy

--- InkomsApp/InkomsApp/settings.py
+++ InkomsApp/InkomsApp/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-&pxxz*pl5=22%yd2xm@zkr@_v+$b4_bz0w(rktn-qn@@+-h*s*
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     # '*',
